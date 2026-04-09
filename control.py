792c2dc39f2b8fbe617b70f3204cd3eb6508a21d
vinotha2007-V <logiravi2002@gmail.com>
2026-04-09 21:25:32 +0530

    Create control.py

---
+++ control.py
@@ -0,0 +1,9 @@
+def control_robot(direction):
+    if direction == "FORWARD":
+        print("Moving Forward")
+    elif direction == "LEFT":
+        print("Turning Left")
+    elif direction == "RIGHT":
+        print("Turning Right")
+    elif direction == "STOP":
+        print("Stopping")
